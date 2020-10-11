4ce7007fbb84b45b52941433537f5103d26c2a7f
shivamj2603 <shivamj2603@gmail.com>
2020-10-12 04:52:35 +0530

    Added:Functionality to find maximum among 3 Float objects

--- main/java/com/generics/findmax/FindMax.java
+++ main/java/com/generics/findmax/FindMax.java
@@ -2,7 +2,7 @@ package com.generics.findmax;
 
 public class FindMax {
 	/**
-	 * Usecase 1 Function returns the maximum Integer
+	 * Usecase 1 : Function returns the maximum Integer
 	 * 
 	 * @param num1
 	 * @param num2
@@ -11,6 +11,23 @@ public class FindMax {
 	 */
 	public static Integer findMaxInt(Integer num1, Integer num2, Integer num3) {
 		Integer max = num1;
+		if (num2.compareTo(max) > 0) {
+			max = num2;
+		}
+		if (num3.compareTo(max) > 0) {
+			max = num3;
+		}
+		return max;
+	}
+	/**
+	 * UseCase 2 : Function returns the maximum Float
+	 * @param num1
+	 * @param num2
+	 * @param num3
+	 * @return
+	 */
+	public static Float findMaxInt(Float num1, Float num2, Float num3) {
+		Float max = num1;
 		if (num2.compareTo(max) > 0) {
 			max = num2;
 		}
