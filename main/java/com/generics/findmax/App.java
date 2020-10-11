ef25dcbc439aaa5ae49e4420a951137e3b0b1452
shivamj2603 <shivamj2603@gmail.com>
2020-10-12 04:09:04 +0530

    Welcome to Generics

---
+++ main/java/com/generics/findmax/App.java
@@ -0,0 +1,13 @@
+package com.generics.findmax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
