778466b07cdac77d11b24e32fa9f0d72bb5aabed
shivamj2603 <shivamj2603@gmail.com>
2020-10-12 07:00:25 +0530

    Refactored:Improved code readability

--- main/java/com/generics/findmax/FindMax.java
+++ main/java/com/generics/findmax/FindMax.java
@@ -4,47 +4,49 @@ import java.util.Collections;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-public class FindMax<E extends Comparable <E>>{
+public class FindMax<E extends Comparable<E>> {
 	E parameters[];
+
 	/**
-	 * UseCase 3
-	 * Refactored2: Generic class with parametrized Constructor
+	 * UseCase 3 Refactored2: Generic class with parametrized Constructor
+	 * 
 	 * @param first
 	 * @param second
 	 * @param third
 	 */
-	public FindMax(E ... parameters) {
+	public FindMax(E... parameters) {
 		this.parameters = parameters;
 	}
+
 	/**
-	 * Usecase 3
-	 * Function invokes the generic method findMax
+	 * Usecase 3 Function invokes the generic method findMax
+	 * 
 	 * @return
 	 */
 	public E testMaximum() {
 		E max = findMax(parameters);
-		printMax(max); 
+		printMax(max);
 		return max;
 	}
 
 	/**
-	 * UseCase3 
-	 * Refactored1: Generic function to test for maximum
+	 * UseCase3 Refactored1: Generic function to test for maximum
+	 * 
 	 * @param <E>
 	 * @param first
 	 * @param second
 	 * @param third
 	 * @return
 	 */
-	public static <E extends Comparable<E>> E findMax(E ... parameters) {
+	public static <E extends Comparable<E>> E findMax(E... parameters) {
 		Stream<E> stream = Stream.of(parameters);
-		E max = stream.sorted().reduce((first,second) -> second).orElse(null);
+		E max = stream.sorted().reduce((first, second) -> second).orElse(null);
 		return max;
-		
 	}
+
 	/**
-	 * Usecase 5
-	 * Function is a generic method and prints the maximum value
+	 * Usecase 5 Function is a generic method and prints the maximum value
+	 * 
 	 * @param <E>
 	 * @param max
 	 */
@@ -52,4 +54,3 @@ public class FindMax<E extends Comparable <E>>{
 		System.out.println(max);
 	}
 }
-
--- test/java/com/generics/findmax/FindMaxTest.java
+++ test/java/com/generics/findmax/FindMaxTest.java
@@ -11,7 +11,7 @@ class FindMaxTest {
 		Integer first = 3;
 		Integer second = 2;
 		Integer third = 1;
-		assertEquals(first, new FindMax<Integer>(first,second,third).testMaximum());
+		assertEquals(first, new FindMax<Integer>(first, second, third).testMaximum());
 	}
 
 	@Test
@@ -19,7 +19,7 @@ class FindMaxTest {
 		Integer first = 2;
 		Integer second = 3;
 		Integer third = 1;
-		assertEquals(second, new FindMax<Integer>(first,second,third).testMaximum());
+		assertEquals(second, new FindMax<Integer>(first, second, third).testMaximum());
 	}
 
 	@Test
@@ -27,57 +27,57 @@ class FindMaxTest {
 		Integer first = 2;
 		Integer second = 1;
 		Integer third = 3;
-		assertEquals(third, new FindMax<Integer>(first,second,third).testMaximum());
+		assertEquals(third, new FindMax<Integer>(first, second, third).testMaximum());
 	}
-	
+
 	@Test
 	public void givenFirstMaxFloat_shouldReturn_first() {
 		Float first = 3.1f;
 		Float second = 2.1f;
 		Float third = 1.1f;
-		assertEquals(first, new FindMax<Float>(first,second,third).testMaximum());
+		assertEquals(first, new FindMax<Float>(first, second, third).testMaximum());
 	}
-	
+
 	@Test
 	public void givenSecondMaxFloat_shouldReturn_Second() {
 		Float first = 2.1f;
 		Float second = 3.1f;
 		Float third = 1.1f;
-		assertEquals(second, new FindMax<Float>(first,second,third).testMaximum());
+		assertEquals(second, new FindMax<Float>(first, second, third).testMaximum());
 	}
-	
+
 	@Test
 	public void givenThirdMaxFloat_shouldReturn_Third() {
 		Float first = 2.1f;
 		Float second = 1.1f;
 		Float third = 3.1f;
-		assertEquals(third, new FindMax<Float>(first,second,third).testMaximum());
+		assertEquals(third, new FindMax<Float>(first, second, third).testMaximum());
 	}
-	
+
 	@Test
 	public void givenFirstMaxString_shouldReturn_First() {
 		String first = "Peach";
 		String second = "Banana";
 		String third = "Apple";
-		assertEquals(first, new FindMax<String>(first,second,third).testMaximum());
+		assertEquals(first, new FindMax<String>(first, second, third).testMaximum());
 	}
-	
+
 	@Test
 	public void givenSecondMaxString_shouldReturn_Second() {
 		String first = "Banana";
 		String second = "Peach";
 		String third = "Apple";
-		assertEquals(second, new FindMax<String>(first,second,third).testMaximum());
+		assertEquals(second, new FindMax<String>(first, second, third).testMaximum());
 	}
-	
+
 	@Test
 	public void givenThirdMaxString_shouldReturn_Third() {
 		String first = "Banana";
 		String second = "Apple";
 		String third = "Peach";
-		assertEquals(third, new FindMax<String>(first,second,third).testMaximum());
+		assertEquals(third, new FindMax<String>(first, second, third).testMaximum());
 	}
-	
+
 	@Test
 	public void givenMultipeInt_shouldReturn_maxInt() {
 		Integer first = 1;
@@ -104,5 +104,4 @@ class FindMaxTest {
 		String fourth = "coconut";
 		assertEquals(first, new FindMax<String>(first, second, third, fourth).testMaximum());
 	}
-
 }
